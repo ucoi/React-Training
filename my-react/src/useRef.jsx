89378ec649f5ed2aaf5400d9c0eda686e84b2000
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-01-25 12:28:29 +0200

    done

--- my-react/src/useRef.jsx
+++ my-react/src/useRef.jsx
@@ -1,3 +1,48 @@
-function UseReef(){}
+import { useRef, useState, useEffect } from "react";
+function UseReef() {
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
+
+  useEffect(() => {
+    console.log("component rendered ");
+  });
+
+  function handleClick1() {
+    inputRef1.current.focus();
+    inputRef1.current.style.backgroundColor = "red";
+    inputRef2.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "";
+  }
+
+  function handleClick2() {
+    inputRef2.current.focus();
+    inputRef2.current.style.backgroundColor = "black";
+    inputRef1.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "";
+  }
+
+  function handleClick3() {
+    inputRef3.current.focus();
+    inputRef3.current.style.backgroundColor = "green";
+    inputRef1.current.style.backgroundColor = "";
+    inputRef2.current.style.backgroundColor = "";
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={handleClick1}>Click me 1</button>
+        <input ref={inputRef1} type="text" />
+        <br />
+        <button onClick={handleClick2}>Click me 2</button>
+        <input ref={inputRef2} type="text" />
+        <br />
+        <button onClick={handleClick3}>Click me 3</button>
+        <input ref={inputRef3} type="text" />
+      </div>
+    </>
+  );
+}
 
 export default UseReef;
